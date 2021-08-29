431ff0aba31d048dd925799b8ca349805abbddbc
Jason <55802733+ShadowViper@users.noreply.github.com>
2021-08-28 22:42:13 -0400

    Add files via upload

---
+++ medp331/class_1/script.js
@@ -0,0 +1,14 @@
+if(document.querySelector("#scrollbar"))
+{
+  window.onscroll = function() {
+    myFunction()
+  };
+
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("scrollbar").style.height = scrolled + "%";
+
+  }
+}
